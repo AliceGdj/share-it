f7b3bc3e099160b5674ca403627dd71a1dcd00dc
Alice Grandjean <alice.grandj.49@gmail.com>
2025-06-27 18:11:21 +0200

    added search functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,38 +4,62 @@ import { ThemedView } from '@/components/ThemedView';
 import { Collapsible } from '@/components/Collapsible';
 import { Hello } from '@/components/Hello';
 import { data } from '@/components/__mockData__/data';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [search, setSearch] = useState('');
+
+  const getSearchResults = (search: string) => {
+    if (!search) {
+      return data;
+    };
+    const results = data.filter((item) => {
+      if (
+        item.name.toLowerCase().includes(search.toLowerCase()) ||
+        item.category.toLowerCase().includes(search.toLowerCase()) ||
+        item.platform.toLowerCase().includes(search.toLowerCase()) ||
+        item.recommendedBy.toLowerCase().includes(search.toLowerCase())
+      ) return item;
+    });
+    return results
+  };
+  const dataShown = search ? getSearchResults(search) : data;
+
   return (
     <ThemedView style={styles.stepContainer}>
       <TextInput
-        onChangeText={(search) => console.log('...search happening')}
+        onChangeText={(text) => setSearch(text)}
         style={styles.searchBar}
         placeholder="Search..."
       />
       <ThemedText type="title">Common shared list:</ThemedText>
       <Hello />
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        nestedScrollEnabled={true} // Allows inner FlatList to scroll TODO MAYBE CHANGE THAT (not recommended)
-        style={styles.flatList}
-        renderItem={({ item }) => (
-          <Collapsible title={`${item.name} (${item.recommendedBy})`}>
-            <View>
-              <ThemedText>Category: {item.category}</ThemedText>
-              <ThemedText>Platform: {item.platform}</ThemedText>
-              <ThemedText>Recommended by: {item.recommendedBy}</ThemedText>
-              <ThemedText>Rating: {item.rating}</ThemedText>
-              <ThemedText>
-                Comment: {item.comment}
-                {'\n'}
-                {'\n'}
-              </ThemedText>
-            </View>
-          </Collapsible>
-        )}
-      />
+      { dataShown.length !== 0 ? 
+        <>
+          <FlatList
+            data={dataShown}
+            keyExtractor={(item) => item.id}
+            nestedScrollEnabled={true} // Allows inner FlatList to scroll TODO MAYBE CHANGE THAT (not recommended)
+            style={styles.flatList}
+            renderItem={({ item }) => (
+              <Collapsible title={`${item.name} (${item.recommendedBy})`}>
+                <View>
+                  <ThemedText>Category: {item.category}</ThemedText>
+                  <ThemedText>Platform: {item.platform}</ThemedText>
+                  <ThemedText>Recommended by: {item.recommendedBy}</ThemedText>
+                  <ThemedText>Rating: {item.rating}</ThemedText>
+                  <ThemedText>
+                    Comment: {item.comment}
+                    {'\n'}
+                    {'\n'}
+                  </ThemedText>
+                </View>
+              </Collapsible>
+            )}
+          />
+        </>
+        : <ThemedText>Try another search, no results found...</ThemedText>
+      }
     </ThemedView>
   );
 }
